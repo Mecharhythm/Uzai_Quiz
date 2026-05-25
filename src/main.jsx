07eeb386cb97c488406b4902c5b6f7e4f88710cc
Mecharhythm <dhzyrswwzw@privaterelay.appleid.com>
2026-05-25 13:08:22 +0900

    Add main.jsx to render UzaiQuiz component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UzaiQuiz from "./UzaiQuiz";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <UzaiQuiz />
+  </React.StrictMode>
+);
